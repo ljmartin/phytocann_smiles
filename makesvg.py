247a8c954590e1c9b4c26894b969021521c91905
ljmartin <lewis.martin44@gmail.com>
2021-02-17 14:48:08 +1100

    fixed naming

--- makesvg.py
+++ makesvg.py
@@ -6,7 +6,7 @@ from IPython.display import SVG
 df = pd.read_csv('./phytocannabinoids.tsv', sep='\t')
 smiles = list(df['smiles'])
 names = list(df['name'])
-l = 20
+l = 30
 names = [n if len(n)<l else n[:l-3]+'...' for n in names]
 
 mols = [Chem.MolFromSmiles(i) for i in smiles]
